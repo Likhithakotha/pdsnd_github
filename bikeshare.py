09aeb2353f511c1cb6bc4ac8f1dbd03328e30da5
Likhitha <likhithakotha1997@gmail.com>
2022-11-23 19:05:22 +0530

    refactor: Display thank you message

--- bikeshare.py
+++ bikeshare.py
@@ -192,6 +192,8 @@ def main():
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
+	  else:
+		print("Thank you!!!") 
 
 
 if __name__ == "__main__":
